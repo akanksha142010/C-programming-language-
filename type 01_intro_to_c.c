bfa5d5548cbfa20519a5421df4df571bb7522346
akanksha142010 <shreekripak@gmail.com>
2026-06-02 22:20:57 +0530

    type 01_intro_to_c.c

---
+++ type 01_intro_to_c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+printf("Welcome\n");
+printf("C is a general - purpose, programming language.\n");
+printf("It was developed by Dennis Ritchie in 1972.\n");
+printf("It is called as mid level language because of it has features of both high level + low-level.");
+return 0;
+}
