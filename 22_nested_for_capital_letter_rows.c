f2afa76d5ea0adb9e644479d0f9c3748adc622b9
akanksha142010 <shreekripak@gmail.com>
2026-06-06 22:10:09 +0530

    22_nested_for_capital_letter_rows.c

---
+++ 22_nested_for_capital_letter_rows.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,row,col;
+printf("Enter the value for row = ");
+scanf("%d",&row);
+printf("Enter the value for column = ");
+scanf("%d",&col);
+
+for(i=65; i<=row+65; i++)
+{
+for(j=1; j<=col+65; j++)
+  printf("%c\t",i);
+printf("\n");
+}
+return 0;
+}
