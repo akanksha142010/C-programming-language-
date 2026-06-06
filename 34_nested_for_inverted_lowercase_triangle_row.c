8ab8d02a4ec1d4b35ae9a602f90636fc4528c7e7
akanksha142010 <shreekripak@gmail.com>
2026-06-07 00:52:32 +0530

    34_nested_for_inverted_lowercase_triangle_row.c

---
+++ 34_nested_for_inverted_lowercase_triangle_row.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,nor,noc;
+
+printf("Enter the row number = ");
+scanf("%d",&nor);
+printf("Enter the column number = ");
+scamf("%d", &noc);
+for(i=1; i<=nor; i++)
+{
+for(j=1; j<=96(noc-i); j++)
+  printf("%c",i);
+printf("\n");
+}
+return 0;
+}
