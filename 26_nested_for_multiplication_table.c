d853b3ccd18ab2a7af40fa464cdb050464a42e22
akanksha142010 <shreekripak@gmail.com>
2026-06-06 23:41:39 +0530

    26_nested_for_multiplication_table.c

---
+++ 26_nested_for_multiplication_table.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,row,col;
+printf("Enter the value for row = ");
+scanf("%d",&row);
+printf("Enter the value for column = ");
+scanf("%d",&col);
+
+for(i=1; i<=row; i++)
+{
+for(j=1; j<=col; j++)
+  printf("%d\t",i*j);
+printf("\n");
+}
+return 0;
+}
