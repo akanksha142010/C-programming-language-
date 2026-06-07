cad057e7dcdfa7d7cbfbd6331a0097f06682de5c
akanksha142010 <shreekripak@gmail.com>
2026-06-07 23:31:33 +0530

    38_triple_nested_uppercase_letter_pyramid.c

---
+++ 38_triple_nested_uppercase_letter_pyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,k,nor,noc;
+printf("Enter the value for row =");
+scanf("%d", &nor);
+printf("Enter the value for column =");
+scanf("%d", &noc);
+for(i=1; i<=nor; i++)
+{
+for(j=1; j<=noc-i; j++)
+  printf(" ");
+{
+for(k=1; k<=65+(2*i-1); k++)
+  printf("%c",k);
+printf("\n");
+}
+}
+return 0;
+}
